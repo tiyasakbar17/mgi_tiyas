d30994e1384e9b1ad45e59c7a265d0e5198b5958
Muhammad Tiyas Fachreza Akbar <tiyas.akbar@gmail.com>
2021-01-12 23:20:15 +0700

    fixing

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  nama: String,
-  hobi: String,
-  alamat: String,
-  nomor_telp: Number,
+  nama: {
+    type: String,
+    required: true,
+  },
+  hobi: {
+    type: String,
+    required: true,
+  },
+  alamat: {
+    type: String,
+    required: true,
+  },
+  nomor_telp: {
+    type: Number,
+    required: true,
+  },
 });
 
 const User = mongoose.model("users", userSchema);
